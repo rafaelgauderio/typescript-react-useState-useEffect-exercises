f229499eb5800c9eeef07b0e90dbc9d3499c596b
Rafael de Luca <deluca1712@gmail.com>
2023-09-01 11:33:21 -0300

    solution using useState - yet not ideal solution

--- exercise-useeffect/src/App.tsx
+++ exercise-useeffect/src/App.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import './App.css'
 import { findEmployee } from './data';
 
 function App() {
 
   // WRONG FORM
-
+  /*
   let employeeId = 1;
   let employee = findEmployee(employeeId);
+  */
+
+  // USING useState
+
+  let [employeeId, setEmployeeId] = useState(1);
+  const [employee, setEmployee] = useState(findEmployee(employeeId));
 
   function changeEmployeeId() {
-    employeeId++;
-    employee = findEmployee(employeeId);
+    setEmployeeId(employeeId++);
+    setEmployee(findEmployee(employeeId));
     console.log("conde pass on changeEmployeeId");
   }
 
